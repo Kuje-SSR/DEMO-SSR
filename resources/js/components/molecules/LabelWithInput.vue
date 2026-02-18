20bb7ce551424ce553da24a7b5f522760e7d42b6
Kuje SSR <nickolai.gacus@splitsecondresearch.com>
2026-02-18 06:12:58 -0800

    Initialized Project

---
+++ resources/js/components/molecules/LabelWithInput.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <Label/>
+        <Input/>
+    </div>
+</template>
+
+<script setup>
+    import Input from "@/components/atoms/input.vue"
+    import Label from "@/components/atoms/label.vue"
+</script>
---
+++ resources/js/pages/MyPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <Head title="MyPage" />
+
+    <AppLayout>
+        <div
+            class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
+        >
+            
+            My Page
+        </div>
+    </AppLayout>
+</template>
+
+<script setup>
+</script>
